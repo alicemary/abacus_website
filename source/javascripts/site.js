54f60693f9ac45badea3e103f3711f36cc36cab0
Alice green <alicemary2011@gmail.com>
2019-01-14 10:59:53 -0500

    margins

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,2 +1,10 @@
-// This is where it all goes :)
+// menu toggle
+
+
+// (function (window, document) {
+//       document.getElementById('toggle').addEventListener('click', function (e) {
+//           document.getElementById('tuckedMenu').classList.toggle('custom-menu-tucked');
+//           document.getElementById('toggle').classList.toggle('x');
+//       });
+//       })(this, this.document);
 

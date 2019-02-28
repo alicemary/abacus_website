f85a94f4de3d86a6be90bf2d3f34edfdca106e0e
Alice green <alicemary2011@gmail.com>
2019-02-28 11:53:20 -0500

    modal button

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,42 +1,137 @@
+// Main menu toggle
 
 
-const MENU = document.querySelectorAll(".menu-margin a:not(:last-child)");
+// selectors
+const MENU = document.querySelectorAll(".menu-margin > li:not(:last-child) > a");
+
+// const  LIST= document.querySelectorAll(".menu-margin > li:not(:last-child) > a");
+
+const  LIST= document.querySelectorAll(".menu-margin > li:not(:last-child)");
+
 
 const HAMBURGER = document.querySelector(".hamburger");
 
-// const bars = HAMBURGER.querySelectorAll(".bar");
+const CLICKBURGER = document.querySelector(".click-hamburger");
+
+const DROPDOWN = document.querySelector("#levels ul");
+
+const LEVELS = document.querySelector("#levels");
+
+// When the page is loaded, the hide class is added to each menu item. The default (when javascript is not working) will be that the menu is visible.
 
 
-MENU.forEach((item) => {
-    console.log(item.classList.add("hide"));
+LIST.forEach((item) => {
+    item.classList.add("hide");
 });
 
 
 
-// When the hamburger is clicked, the hide class is added to the menu anchors
+// Adds and removes the hide class from menu items so they are not visible when the hamburger menu is clicked.
+
 function toggleHide(){
-	MENU.forEach((item) => {
+	LIST.forEach((item) => {
     	item.classList.toggle("hide");
 	});
 }
 
-// When the mouse goes over the hamburger, the bars change color
-// function highlight(){
-// 	bars.forEach((item) => {
-//     	item.classList.toggle("highlight");
-// 	});
-// }
 
+// Adds the active class to the hamburger menu causing it to change into a cross
 function cross(){
 	HAMBURGER.classList.toggle("active");
 }
 
 
-HAMBURGER.addEventListener("click", toggleHide, false);
-HAMBURGER.addEventListener("click", cross, false);
+CLICKBURGER.addEventListener("click", toggleHide, false);
+CLICKBURGER.addEventListener("click", cross, false);
 
-// HAMBURGER.addEventListener("mouseover", highlight, false);
+CLICKBURGER.addEventListener("keypress", toggleHide, false);
+CLICKBURGER.addEventListener("keypress", cross, false);
 
-// HAMBURGER.addEventListener("mouseout", highlight, false);
+
+
+
+
+
+
+
+
+
+
+// highlight the menu item of the current page
+let page = document.querySelector("content");
+
+
+
+MENU.forEach( function(item) {
+	if (item.innerHTML == page.classList.value){
+		item.classList.add("highlight");
+	}
+})
+
+
+
+
+
+
+
+// custom number slider
+// let slider = document.getElementById("numberRange");
+
+// let output = document.getElementById("numbers");
+
+// output.innerHTML = slider.value;
+
+// slider.oninput = function(){
+// 	output.innerHTML = this.value;
+// }
+
+
+// custom digit slider
+// let slider2 = document.getElementById("digitRange");
+
+// let output2 = document.getElementById("digits");
+
+// output2.innerHTML = slider2.value;
+
+// slider2.oninput = function(){
+// 	output2.innerHTML = this.value;
+// }
+
+
+
+// modal
+
+
+let button = document.querySelector(".press-button button");
+let modal = document.querySelector('#id01');
+let modalCross = document.querySelector(".modal-x");
+
+
+
+// open the modal when a button is clicked or enter pressed
+function showModal(){
+	console.log("showModal called");
+	document.getElementById('id01').style.display='block';
+	console.log("showModal complete");
+}
+
+
+
+
+button.addEventListener("click", showModal, false);
+// button.addEventListener("keypress", showModal, false);
+
+
+
+// close modal by clicking or pressing enter on the x
+function closeModal(){
+	console.log("closeModal called");
+	document.getElementById('id01').style.display='none';
+	console.log("done");
+	
+}
+
+modalCross.addEventListener("click", closeModal, false);
+// modalCross.addEventListener("keypress", closeModal, false);
 
 console.log("hi");
